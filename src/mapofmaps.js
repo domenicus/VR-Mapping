0ea16553c47a20a793e6540aca501346b0497a57
groomble <groomblecom@gmail.com>
2019-01-15 10:02:16 -0500

    Add map of maps

--- src/mapofmaps.js
+++ src/mapofmaps.js
@@ -1,29 +1,34 @@
 "use strict";
 AFRAME.registerComponent("mapofmaps", {
+	zones: null,
+	selected: false,
 	init: function initMapOfMaps() {
-		var lmaps = document.querySelectorAll(".lmap");
+		var el = this.el;
 		var self = this;
-		for(var i = 0; i < lmaps.length; i++) {
-			var t = i;
-			/*
-			lamps[i].addEventListener('mousedown',
-				function(e) {
-				*/
-		}
-	},
-	tick: function tickMapOfMaps() {
-		var lmaps = document.querySelectorAll(".lmap");
-		var camera = document.querySelector("a-camera");
-		if(!camera) {
-			camera = document.querySelector("[camera]");
-		}
-		if(!camera) {
-			console.log("failed:");
-			return;
-		}
-		var p = camera.object3D.position;
-		for(var i = 0; i < lmaps.length; i++) {
-			lmaps[i].object3D.lookAt(p);
-		}
+		console.log(el);
+		el.addEventListener('click', function(e) {
+			console.log("Clicked", e);
+			if(self.data.selected) {
+				el.setAttribute("src", "temp/out.jpg");
+				self.data.selected = false;
+				return;
+			}
+			if(self.data.zones) {
+				var zones = self.data.zones;
+				var isect = e.detail.intersection.uv;
+				for(var i = 0; i < zones.length; i++) {
+					var coords = zones[i].coords;
+					if(coords[0] < isect.x && coords[2] > isect.x &&
+						coords[1] < isect.y && coords[3] > isect.y) {
+						console.log(zones[i].name);
+						el.setAttribute("src", "temp/flipped."+zones[i].name);
+						self.data.selected = true;
+					}
+				}
+			}
+		});
+		fetch("temp/out.json")
+			.then(response => response.json())
+			.then(json => self.data.zones = json);
 	}
 });
---
+++ src/temp/makemap.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+import json
+import math
+from PIL import Image
+import PIL
+
+image_names = ["7962e.jpg", "14349-000.jpg", "32175.jpg"];
+images = [];
+
+for name in image_names:
+    images.append(Image.open(name).transpose(PIL.Image.FLIP_LEFT_RIGHT));
+
+#make working canvas:
+working = Image.new('RGB', (8192, 4096), color=(73, 109, 137))
+
+#project images into a band around the equator
+
+total_w = 0
+for im in images:
+    total_w += im.size[0]
+
+incr_x = total_w/8192
+
+i = 0;
+x = 0;
+for lng in range(0,8191):
+    x += incr_x;
+    if x+incr_x >= images[i].size[0]:
+        x -= images[i].size[0]
+        i += 1
+    if i >= len(images):
+        break
+    print(i)
+    c_height = images[i].size[1]
+    c_diff = int((4096-c_height)/2)
+    imslice = images[i].crop(box=(int(x), 0, int(x+incr_x), c_height))
+    working.paste(imslice, (lng, c_diff, imslice.size[0]+lng, imslice.size[1]+c_diff))
+
+#Working is now a lat-long image; next, to turn it into a projection.
+working.save("out.jpg");
+
+#Also make a JSON of 'hit zones' kinda deal
+zones = []
+x = 0
+for i in range(0, len(images)):
+    c_height = images[i].size[1]
+    c_diff = int((4096-c_height)/2)
+    zones.append({"name":image_names[i], "coords":
+        (x/total_w, c_diff/4096, (x+images[i].size[0])/total_w, (c_diff+c_height)/4096)});
+    x += images[i].size[0]
+    images[i].save("flipped."+image_names[i]);
+
+with open('out.json', 'w') as jsonout:
+    json.dump(zones, jsonout)
+
+print("Done!")
